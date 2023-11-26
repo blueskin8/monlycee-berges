a2cd49ebdaaaa1ecffb8a7edce8418a71453c012
DemonloupYT <demonloup2313@gmail.com>
2023-11-26 19:49:24 +0100

    Check For Update
    - vérifie si l'app a la même version que la version latest sur GitHub

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:package_info/package_info.dart';
+import 'package:github/github.dart';
 import 'package:monlycee/pages/home_page.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -6,6 +9,21 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final SharedPreferences prefs = await SharedPreferences.getInstance();
   runApp(MonLycee(prefs: prefs));
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  checkForUpdate();
+}
+
+void checkForUpdate() async {
+  PackageInfo packageInfo = await PackageInfo.fromPlatform();
+  final latestRelease = await GitHub().repositories.getLatestRelease(RepositorySlug('blueskin8', 'monlycee-berges'));
+  final latestVersion = latestRelease.tagName;
+  String appVersion = "v${packageInfo.version}";
+  if (latestVersion == appVersion) {
+    debugPrint("c'est la meme chose");
+  } else {
+    debugPrint("$packageInfo | $appVersion");
+  }
 }
 
 class MonLycee extends StatelessWidget {
